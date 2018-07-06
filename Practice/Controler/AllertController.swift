ee2c3e13a1c6e422dc7f1505bd87d944fd04eb33
Victor Yanuchkov <victor.yanuchkov@dev-minimac.local>
2018-07-06 17:48:49 +0300

    Add extension for UIAlertController

---
+++ Practice/Controler/AllertController.swift
@@ -0,0 +1,20 @@
+//
+//  AllertController.swift
+//  Practice
+//
+//  Created by Victor Yanuchkov on 06/07/2018.
+//  Copyright © 2018 Victor Yanuchkov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIAlertController{
+    func validateAlert(title: String, message: String) -> UIAlertController{
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+         alert.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: "Default actio"), style: .default, handler: nil))
+        
+        return alert
+    }
+}
+
