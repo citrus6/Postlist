9721946a33e2d79ed378e597d9d8d063d5d4de98
Victor Yanuchkov <victor.yanuchkov@minimac.sf-office>
2018-07-09 16:50:43 +0300

    Add loading status

--- Practice/Controler/ViewController.swift
+++ Practice/Controler/ViewController.swift
@@ -8,19 +8,30 @@ struct CellDate {
 class TableViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    weak var activityIndicatorView: UIActivityIndicatorView!
     
     var data = [NetworkManager.Item]()
 
-    override func prepare(for seque: UIStoryboardSegue, sender: Any?){
-        if seque.identifier == "showPost" {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "showPost" {
             let cell = sender as! UITableViewCell
             if let indexPath = tableView.indexPath(for: cell){
-               let itemDescription = seque.destination as! ItemDescriptionViewController
+               let itemDescription = segue.destination as! ItemDescriptionViewController
                 itemDescription.postId = data[indexPath.row].id
-                itemDescription.postTitle = data[indexPath.row].title
-                itemDescription.body = data[indexPath.row].body
-       
+                itemDescription.postTitle = data[indexPath.row].title!
+                itemDescription.body = data[indexPath.row].body!
             }
+        } else if segue.identifier == "newPost" {
+            let navigationController = segue.destination as! UINavigationController
+            
+            let addView = navigationController.topViewController as! AddNewPost
+            addView.onAddNewPost = {(title, body) in
+                let id = self.data.count + 1
+                let item = NetworkManager.Item(id: id, title: title, body: body)
+                self.data.append(item)
+                self.tableView.reloadData()
+            }
+            
         }
     }
     
@@ -29,12 +40,30 @@ class TableViewController: UIViewController, UITableViewDataSource {
   
         tableView.dataSource = self
         NetworkManager.getItems(view: self)
-        
+  
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        tableView.backgroundView = activityIndicatorView
+        tableView.separatorStyle = .none
+        self.activityIndicatorView = activityIndicatorView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if data.count == 0 {
+            tableView.separatorStyle = .none
+            activityIndicatorView.startAnimating()
+        } else {
+            tableView.separatorStyle = .singleLine
+        }
     }
     
     func updateData(){
         tableView.separatorStyle = UITableViewCellSeparatorStyle.singleLine
+        if data.count == 0{
+            activityIndicatorView.stopAnimating()
+        }
         UIView.transition(with: tableView, duration: 0.35, options: .transitionFlipFromTop, animations: {self.tableView.reloadData()}, completion: nil)
+        
     }
     
 
