fd390380ce0301531d4c4634d8ae7ef562ee8626
Victor Yanuchkov <victor.yanuchkov@minimac.sf-office>
2018-07-10 15:02:30 +0300

    Add loginVC

---
+++ Practice/Controler/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Practice
+//
+//  Created by Victor Yanuchkov on 10/07/2018.
+//  Copyright © 2018 Victor Yanuchkov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
